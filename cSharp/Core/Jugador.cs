344efbbf075546be78b29716902fc5fc863ca80d
Armoaaa <lorenzo.tedesco.et12de1@gmail.com>
2025-12-01 15:23:35 -0300

    ola

--- cSharp/Core/Jugador.cs
+++ cSharp/Core/Jugador.cs
@@ -1,14 +1,18 @@
+using System;
+using System.Collections.Generic;
+
 namespace Core;
+
 public class Jugador
 {
     public int id_jugador { get; set; }
     public string nombre { get; set; } = string.Empty;
     public string apellido { get; set; } = string.Empty;
-    public string? apodo { get; set; } // opcional
+    public string? apodo { get; set; }
     public DateTime fecha_nacimiento { get; set; }
     public decimal cotizacion { get; set; }
-    public Equipo Equipo { get; set; }
+    public int id_equipo { get; set; }
     public int id_tipo { get; set; } // FK -> TipoJugador
-    public IEnumerable<Puntuacion> puntos = [];
+    public List<Puntuacion>? puntuaciones { get; set; }
 }
 
